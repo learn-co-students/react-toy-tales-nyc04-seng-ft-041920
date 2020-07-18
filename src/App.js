33abf92b4b336ea875ef879a461276250afb4fd8
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-18 18:56:27 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
+// import data from './data'
 
 
 class App extends React.Component{
-
   state = {
+    toys: [],
     display: false
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/toys")
+    .then(r => r.json())
+    .then(toysArray => {
+      this.setState({toys: toysArray})
+    })
   }
 
   handleClick = () => {
@@ -21,20 +29,88 @@ class App extends React.Component{
     })
   }
 
-  render(){
+  handleAddToy = (obj) => {
+    this.setState(prevState => {
+      return {
+        toys: [
+          ...prevState.toys,
+          obj
+        ]
+      }
+    }, console.log(this.state.toys))
+  }
+
+  handleRemoveToy = (id) => {
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(() => {
+      // maybe we do need a fetch helper
+      fetch("http://localhost:3000/toys")
+      .then(r => r.json())
+      .then(toysArray => {
+        this.setState({toys: toysArray})
+      })
+    })
+  }
+
+  handleLikeToy = (id, likes) => {
+    // need to get current likes
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({"likes": likes + 1})
+    }).then(r => r.json())
+    .then(() => {
+      // maybe we do need a fetch helper
+      fetch("http://localhost:3000/toys")
+      .then(r => r.json())
+      .then(toysArray => {
+        this.setState({toys: toysArray})
+      })
+    })
+
+//     likesSpan.innerText = likes + 1
+// fetch(`http://localhost:3000/toys/${toy.id}`,
+//   {
+//     method: "PATCH",
+//     body: JSON.stringify({"likes" : likes + 1}),
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json"
+//     }
+//   }
+// )
+// .then(resp => resp.json())
+// .then(toy => {
+//    console.log(toy.likes)
+// })
+
+  }
+
+  render() {
     return (
       <>
         <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
+        { this.state.display ?
+          <ToyForm
+            addToy={this.handleAddToy}
+          />
+          :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer
+          likeToy={this.handleLikeToy}
+          removeToy={this.handleRemoveToy}
+          toyData={this.state.toys}
+        />
       </>
     );
   }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = ({toyData, removeToy, likeToy}) => {
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {toyData.map((toy, index) =>
+        <ToyCard
+          likeToy={likeToy}
+          removeToy={removeToy}
+          name={toy.name}
+          image={toy.image}
+          likes={toy.likes}
+          id={toy.id}
+          key={index}
+        />
+      )}
     </div>
   );
 }
