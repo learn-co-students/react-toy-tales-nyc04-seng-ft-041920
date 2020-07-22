e434e42a16d281531bc0fa991f4595b2fa664af9
hkofkin <hkofkin@gmail.com>
2020-07-22 01:42:09 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,16 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/toys')
+      .then(r => r.json())
+      .then(toys => this.setState({
+        toys: toys
+      }))
   }
 
   handleClick = () => {
@@ -21,20 +30,75 @@ class App extends React.Component{
     })
   }
 
+  addToy = (e) => {
+    e.preventDefault()
+    const newToy = {
+      name: e.target.name,
+      image: e.target.image,
+      likes: 0
+    }
+
+    fetch(`http://localhost:3000/toys`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+      .then(r => r.json())
+      .then(toy => this.setState({
+        toys: [...this.state.toys, toy]
+      }))
+  }
+
+  removeToy = toyToRemove => {
+    const updatedToyData = this.state.toys.filter(toy => toy.id !== toyToRemove.id)
+
+    this.setState({
+      toys: updatedToyData
+    })
+
+    fetch(`http://localhost:3000/toys/${toyToRemove.id}`, {
+      method: "DELETE"
+    })
+  }
+
+  addLike = toyToAddLike => {
+    const likedToy = this.state.toys.filter(toy => toy.id === toyToAddLike.id)
+    const toyIndex = this.state.toys.findIndex(toy => toy.id === toyToAddLike.id )
+    const updatedData = [...this.state.toys]
+    updatedData[toyIndex] = {...updatedData[toyIndex], likes: toyToAddLike.likes += 1}
+    this.setState({
+      toys: updatedData
+    })
+
+    fetch(`http://localhost:3000/toys/${toyToAddLike.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(toyToAddLike)
+    })
+      .then(r => r.json())
+      .then(updatedToy => updatedToy.likes += 1)
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm addToy={this.addToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} removeToy={this.removeToy} addLike={this.addLike}/>
       </>
     );
   }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
+
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {props.toys.map(toy => <ToyCard key={toy.id} toy={toy} removeToy={props.removeToy} addLike={props.addLike}/>)}
     </div>
   );
 }
